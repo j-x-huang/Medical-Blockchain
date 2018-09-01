81873911859da1034158ee8291095c9b81059df3
Jack Huang <huangxjack@gmail.com>
2018-09-01 19:45:31 +1200

    added notifications, key sharing and revokations

--- composer-artifacts/lib/logic.js
+++ composer-artifacts/lib/logic.js
@@ -156,16 +156,6 @@ function makeid() {
     return text;
 }
 
-function makeid() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
-    for (var i = 0; i < 7; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-  
-    return text;
-}
-
 function checkAccessToPatient(patient, healthProvider) {
     
     let consentedHPs = patient.consentedHPs;
--- patient-app/js/angular-module.js
+++ patient-app/js/angular-module.js
@@ -20,8 +20,6 @@ app.controller('myCtrl', function ($scope, $http, $websocket, ModalService) {
     $scope.patientKey
     $scope.privateKey
 
-    $scope.selectedRecord = {}
-    $scope.types = ["Allergy", "Procedure", "Observation", "Medication", "Immunization", "Condition"]
 
     $scope.shareForm = {
         patient: "",
@@ -29,7 +27,8 @@ app.controller('myCtrl', function ($scope, $http, $websocket, ModalService) {
     }
 
     $scope.myArray = []
-
+    $scope.hpArray = []
+    $scope.notiTable = []
 
     $scope.getPatient = function () {
         if ($scope.pid === '' || $scope.pid === undefined) {
@@ -45,14 +44,23 @@ app.controller('myCtrl', function ($scope, $http, $websocket, ModalService) {
         }, _error)
     }
 
-    $scope.shareKey = function () {
+    $scope.shareKey = function (index) {
 
+        if ($scope.pid === '' || $scope.pid === undefined) {
+            alert("Your ID is not stated")
+            return
+        }
 
-        var hid = $scope.shareForm.healthProvider
+        if ($scope.patientKey === '' || $scope.patientKey === undefined) {
+            alert("You have not entered your patient key")
+            return
+        }
+
+        var hid = $scope.notiTable[index].hid
 
         $scope.shareForm.$class = "nz.ac.auckland.ShareKey"
-        $scope.shareForm.patient = "resource:" + namespace + ".Patient#" + $scope.shareForm.patient
-        $scope.shareForm.healthProvider = "resource:" + namespace + ".HealthProvider#" + $scope.shareForm.healthProvider
+        $scope.shareForm.patient = "resource:" + namespace + ".Patient#" + $scope.pid
+        $scope.shareForm.healthProvider = "resource:" + namespace + ".HealthProvider#" + hid
 
         $http.get(apiBaseURL + "HealthProvider/" + hid)
             .then(function (response) {
@@ -79,13 +87,54 @@ app.controller('myCtrl', function ($scope, $http, $websocket, ModalService) {
             }, _error)
     }
 
-    $scope.getData = function (tag) {
-        var endpoint = apiBaseURL + tag
-        $scope.endpoint = endpoint
+    $scope.viewKeys = function() {
+        if ($scope.pid === '' || $scope.pid === undefined) {
+            alert("Your ID is not stated")
+            return
+        }
 
-        $http.get(endpoint).then(function (response) {
-            $scope.myArray = response.data
+        $scope.hpArray = []
+
+        var endpoint = endpoint2 + 'selectPatientKeysByPatientID?p=resource%3Anz.ac.auckland.Patient%23' + $scope.pid;
+
+        $http.get(endpoint).then( function (response) {
+            var hps = new Set()
+
+            response.data.forEach(function (data) {
+                var hpLine = data.healthProvider.split('#')
+                hpId = hpLine[1];
+
+                hps.add(hpId)
+            })
+            console.log(hps)
+            hps.forEach( function (hp) {
+                $http.get(apiBaseURL + 'HealthProvider/' + hp).then( function (response) {
+                    $scope.hpArray.push(response.data)
+                }, _error)
+            })
+
         }, _error)
+    }
+
+    $scope.revokeKey = function(index) {
+        var hid = $scope.hpArray[index].id
+
+        var endpoint = apiBaseURL + "RevokeMedicalRecordsSharing"
+
+        var revokeForm = {
+            $class: "nz.ac.auckland.RevokeMedicalRecordsSharing",
+            patient: "resource:" + namespace + ".Patient#" + $scope.pid,
+            healthProvider: $scope.shareForm.healthProvider = "resource:" + namespace + ".HealthProvider#" + hid
+        }
+
+        $http({
+            method: 'POST',
+            url: endpoint,
+            data: angular.toJson(revokeForm),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(_success, _error)
     }
 
     $scope.setTab = function (tag) {
@@ -141,23 +190,6 @@ app.controller('myCtrl', function ($scope, $http, $websocket, ModalService) {
         });
     };
 
-    $scope.viewRecords = function (index) {
-        var patient = $scope.myArray[index]
-
-        ModalService.showModal({
-            templateUrl: "./recordsModal.html",
-            controller: "recordsController",
-            preClose: (modal) => { modal.element.modal('hide'); },
-            inputs: {
-                title: "Patient Details",
-                patient: patient,
-                patientKey: $scope.patientKey
-            }
-        }).then(function (modal) {
-            modal.element.modal();
-        });
-
-    }
 
     $scope.handleFiles = function (files) {
         var file = files[0]
@@ -185,6 +217,8 @@ app.controller('myCtrl', function ($scope, $http, $websocket, ModalService) {
             return
         }
 
+        clearTables();
+
         getRecords(endpoint2 + "selectAllAllergyRecords?p=resource%3Anz.ac.auckland.Patient%23" + $scope.pid, $scope.allergy)
         getRecords(endpoint2 + "selectAllConditionRecords?p=resource%3Anz.ac.auckland.Patient%23" + $scope.pid, $scope.cond)
         getRecords(endpoint2 + "selectAllImmunizationRecords?p=resource%3Anz.ac.auckland.Patient%23" + $scope.pid, $scope.imm)
@@ -209,6 +243,17 @@ app.controller('myCtrl', function ($scope, $http, $websocket, ModalService) {
         }, _error);
     }
 
+    function clearTables() {
+        $scope.allergy=[]
+        $scope.cond=[]
+        $scope.imm=[]
+        $scope.med=[]
+        $scope.obs=[]
+        $scope.proc=[]
+
+        return
+    }
+
     function decryptForm(form) {
         var keys = Object.keys(form)
 
@@ -228,17 +273,34 @@ app.controller('myCtrl', function ($scope, $http, $websocket, ModalService) {
     })
     .$on('$message', function (data) {
         console.log(data)
+        if (data.$class === "nz.ac.auckland.RequestRecordSharingNotification") {
+            var hpLine = data.healthProvider.split('#')
+            var hpId = hpLine[1];
+            console.log(hpLine)
+
+            var pLine = data.patient.split('#')
+            var pId = pLine[1]
+            console.log(pLine)
+
+            var timestamp = new Date(data.timestamp);
+
+            var timeString = timestamp.toLocaleDateString('en-GB') + " @ " + timestamp.toLocaleTimeString('en-GB');
+
+            if (pId === $scope.pid) {
+                var notification = {
+                    hid: hpId,
+                    time: timeString,
+                    Description: "Healthcare provider #" + hpId + " would like to view your records"
+                }
+
+                $scope.notiTable.push(notification)
+                console.log($scope.notiTable)
+            }
+        }
     });
-    
 
-
-    $scope.encryptedPkey
-    $scope.decryptedKey
-
-    $scope.testDecrypt = function() {
-        console.log($scope.encryptedPkey)
-        console.log($scope.privateKey)
-        $scope.decryptedKey = asymDecrypt($scope.encryptedPkey, $scope.privateKey)
-
+    $scope.refresh = function () {
+        $scope.notiTable.reload();
     }
+    
 })
