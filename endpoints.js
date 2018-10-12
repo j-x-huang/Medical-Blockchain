661d21020690b436d5bcd6ab0bba49ec5104a449
Unknown <huangxjack@gmail.com>
2018-10-12 17:41:42 +1300

    changed endpoints

--- endpoints.js
+++ endpoints.js
@@ -1,6 +1,6 @@
 const ADMIN_ENDPOINT = "http://localhost:3000/api/"
 
-const PATIENT_ENDPOINT = "http://localhost:3000/api/"
+const PATIENT_ENDPOINT = "http://localhost:3002/api/"
 
-const HP_ENDPOINT = "http://localhost:3000/api/"
+const HP_ENDPOINT = "http://localhost:3001/api/"
 
